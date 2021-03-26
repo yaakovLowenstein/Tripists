b41b6d3f77ab65efbae0c77b89b88288966a041f
yaakov <ylowenstein@itptek.com>
2021-03-26 14:03:13 -0400

    edits

--- application/models/destinations_model.php
+++ application/models/destinations_model.php
@@ -5,18 +5,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class destinations_model extends CI_Model {
 
     public function getBlogCountries() {
+        $this->db->where('publish', 1);
         $this->db->select("co.*,con.name as continent_name,s.*,b.state", "distinct");
         $this->db->from("blog b");
         $this->db->join("countries co", "co.country_id=b.country");
         $this->db->join("continents con", "co.continent_code=con.code");
-        $this->db->join("states s", "s.state_id=b.state",'left');
+        $this->db->join("states s", "s.state_id=b.state", 'left');
 
         $this->db->order_by("continent_code,name");
 
         $query = $this->db->get();
         return $query->result();
     }
-    public function getBlogCountriesWithoutStates(){
+
+    public function getBlogCountriesWithoutStates() {
+        $this->db->where('publish', 1);
+
         $this->db->distinct();
         $this->db->select("co.*,con.name as continent_name,");
         $this->db->from("blog b");
@@ -25,16 +29,21 @@ class destinations_model extends CI_Model {
         $this->db->order_by("continent_code,name");
 
         $query = $this->db->get();
+
         return $query->result();
     }
-    public function getBlogsInUSA(){
+
+    public function getBlogsInUSA() {
         $this->db->distinct();
         $this->db->where("state!=0");
+        $this->db->where('publish', 1);
         $this->db->select("s.*");
         $this->db->from("blog b");
-        $this->db->join("states s", "s.state_id=b.state",'left');
+        $this->db->join("states s", "s.state_id=b.state", 'left');
 
         $query = $this->db->get();
+            //    print_r($this->db->last_query());die;
+
         return $query->result();
     }
 
